06bb46cff2cfd8a532d1839a229ad4f92c061e49
udz1kor <Nishanth.Gundlapalle@in.bosch.com>
2026-07-01 16:08:31 +0530

    update the ip

--- student-results-ui/src/api/api.js
+++ student-results-ui/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // AXios converts the Javascript into an HTTP request.
 const api = axios.create({
-    baseURL: "http://50.17.121.255:8080"
+    baseURL: "http://34.229.154.246:8080"
 });
 
 export default api;
